980403a5418e7742dac1dbedbc52fa3b6e37a950
vanessaruama <vanessaruama@gmail.com>
2020-10-13 19:02:40 -0300

    Atualização para realizar documentação

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module Calculadora
 
 go 1.15
+
+require golang.org/x/tools v0.0.0-20201013201025-64a9e34f3752 // indirect
--- sub/sub.go
+++ sub/sub.go
@@ -1,5 +1,7 @@
+// Package sub é um pacote para realizar a subtração
 package sub
 
+// Sub faz a subtração de dois números inteiros
 func Sub(a int64, b int64) int64 {
 	return a - b
 }
